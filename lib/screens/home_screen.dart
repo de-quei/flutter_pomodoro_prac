0a9ed39dc9c5e099d3cfd4e3aa795503b35f24b2
hyunji Kim <s2208@e-mirim.hs.kr>
2024-03-26 23:09:14 +0900

    flexible 세팅

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,31 +11,33 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       body: Column(
         children: [
+          // Flexible : 하나의 박스가 얼만큼의 공간을 차지할 지 비율을 정할 수 있음.
+          // Framework와 Flutter Engine이 정확한 비율을 맞춰줌.
+          // width : 400px 같이 하드코딩 할 경우에는 기기 별로 보이는 크기가 다를 수 있음!
           Flexible(
             flex: 1,
             child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.red,
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '25:00',
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
           ),
           Flexible(
-            flex: 2,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.green,
-              ),
-            ),
+            flex: 3,
+            child: Container(),
           ),
           Flexible(
             flex: 1,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
+            child: Container(),
           ),
         ],
       ),
